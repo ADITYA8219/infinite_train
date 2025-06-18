df040e594e48f955798e4354917952914102b6a4
ADITYA8219 <adijaysean90909@gmail.com>
2025-06-19 00:19:42 +0530

    Five pros

---
+++ assignment/p2/p2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 0)
+            Console.WriteLine($"{num} is a positive number");
+        else
+            Console.WriteLine($"{num} is a negative number");
+    }
+}
