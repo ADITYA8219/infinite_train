1c632227360af48dea1316fa49b3c49d0e47b157
ADITYA8219 <121877915+ADITYA8219@users.noreply.github.com>
2025-06-25 18:35:43 +0530

    Add files via upload

---
+++ csharp/cc/testp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static string RemChar(string inp, int pos)
+    {
+        if (pos < 0 || pos >= inp.Length)
+        {
+            return "Invalid position";
+        }
+
+        return inp.Substring(0, pos) + inp.Substring(pos + 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter any line ");
+        string inp = Console.ReadLine();
+
+        Console.Write("which the position which u wanna remove (0 to " + (inp.Length - 1) + "): ");
+        int pos;
+
+        bool isValid = int.TryParse(Console.ReadLine(), out pos);
+
+        if (!isValid || pos < 0 || pos >= inp.Length)
+        {
+            Console.WriteLine("Invalid position.");
+        }
+        else
+        {
+            string result = RemChar(inp, pos);
+            Console.WriteLine("Result: " + result);
+            Console.Read();
+        }
+    }
+}
