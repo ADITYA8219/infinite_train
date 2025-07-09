06bae46fbe1cf7da88040091af79b649c1a4fe01
ADITYA8219 <121877915+ADITYA8219@users.noreply.github.com>
2025-07-09 11:02:53 +0530

    Add files via upload

---
+++ csharp/cc3/cc3_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc3_4
+{ 
+
+    public class Calculator
+    {
+        public delegate int OperationDelegate(int fOperand, int sOperand);
+
+        public static int Add(int fOperand, int sOperand)
+        {
+            return fOperand + sOperand;
+        }
+
+        public static int Subtract(int fOperand, int sOperand)
+        {
+            return fOperand - sOperand;
+        }
+
+        public static int Multiply(int fOperand, int sOperand)
+        {
+            return fOperand * sOperand;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the first integer: ");
+            int fnum = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second integer: ");
+            int snum = int.Parse(Console.ReadLine());
+
+            OperationDelegate addOperation = Add;
+            OperationDelegate subOperation = Subtract;
+            OperationDelegate multiplyOperation = Multiply;
+
+            Console.WriteLine("\nResults");
+            Console.WriteLine($"Addition: {fnum} + {snum} = {addOperation(fnum, snum)}");
+            Console.WriteLine($"Subtraction: {fnum} - {snum} = {subOperation(fnum, snum)}");
+            Console.WriteLine($"Multiplication: {fnum} * {snum} = {multiplyOperation(fnum, snum)}");
+
+            Console.ReadKey();
+        }
+    }
+}
