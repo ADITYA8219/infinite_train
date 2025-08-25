41471d41f0105c50c5a94beeb7fb69e7a132637d
Aditya <adijaysean90909@gmail.com>
2025-08-25 14:53:15 +0530

    outputs

--- MVC/CC/mpro/Controllers/MovieController.cs
+++ MVC/CC/mpro/Controllers/MovieController.cs
@@ -6,17 +6,51 @@ namespace MoviesApp.Controllers
 {
     public class MovieController : Controller
     {
-        private IMovieRepository repo = new MovieRepository();
+        private readonly IMovieRepository repo = new MovieRepository();
 
+  
+        public ActionResult Index()
+        {
+            var movies = repo.GetAll();
+            return View(movies);
+        }
+
+       
+        public ActionResult Filter()
+        {
+            return View();
+        }
+
+       
+        public ActionResult MoviesByDirector(string directorName)
+        {
+            var movies = repo.GetByDirector(directorName);
+            ViewBag.DirectorName = directorName;
+            return View(movies);
+        }
+
+        public ActionResult MoviesByYear(int year)
+        {
+            var movies = repo.GetByYear(year);
+            ViewBag.Year = year;
+            return View(movies);
+        }
+
+        
         public ActionResult Create() => View();
 
         [HttpPost]
         public ActionResult Create(Movie movie)
         {
-            repo.Add(movie);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                repo.Add(movie);
+                return RedirectToAction("Index");
+            }
+            return View(movie);
         }
 
+    
         public ActionResult Edit(int id)
         {
             var movie = repo.GetById(id);
@@ -26,10 +60,15 @@ namespace MoviesApp.Controllers
         [HttpPost]
         public ActionResult Edit(Movie movie)
         {
-            repo.Update(movie);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                repo.Update(movie);
+                return RedirectToAction("Index");
+            }
+            return View(movie);
         }
 
+       
         public ActionResult Delete(int id)
         {
             var movie = repo.GetById(id);
@@ -37,30 +76,10 @@ namespace MoviesApp.Controllers
         }
 
         [HttpPost, ActionName("Delete")]
-        
         public ActionResult DeleteConfirmed(int id)
         {
-            System.Diagnostics.Debug.WriteLine("Deleting movie with ID: " + id);
             repo.Delete(id);
             return RedirectToAction("Index");
-        }
-
-        public ActionResult Index()
-        {
-            var movies = repo.GetAll();
-            return View(movies);
-        }
-
-        public ActionResult MoviesByYear(int year)
-        {
-            var movies = repo.GetByYear(year);
-            return View(movies);
-        }
-
-        public ActionResult MoviesByDirector(string directorName)
-        {
-            var movies = repo.GetByDirector(directorName);
-            return View(movies);
         }
     }
 }
