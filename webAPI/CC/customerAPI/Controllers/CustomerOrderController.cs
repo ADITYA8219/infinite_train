66d771a38b38fe44ab026ef56b57f2b76793274e
Aditya <adijaysean90909@gmail.com>
2025-09-01 13:18:02 +0530

    CC output

---
+++ webAPI/CC/customerAPI/Controllers/CustomerOrderController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Http;
+using CustomerAPI.Models; 
+
+namespace CustomerAPI.Controllers
+{
+    public class CustomerOrderController : ApiController
+    {
+        private Northwind1Entities3 db = new Northwind1Entities3();
+
+        // 1. Get all orders for EmployeeID = 5 (Steven Buchanan)
+        [HttpGet]
+        [Route("api/orders/employee/5")]
+        public IHttpActionResult GetOrdersForStevenBuchanan()
+        {
+            var orders = db.Orders
+                .Where(o => o.EmployeeID == 5)
+                .Select(o => new OrderDTO
+                {
+                    OrderID = o.OrderID,
+                    OrderDate = o.OrderDate,
+                    CompanyName = o.Customer.CompanyName,
+                    ShipCountry = o.ShipCountry
+                })
+                .ToList();
+
+            return Ok(orders);
+        }
+
+        // 2. Call stored procedure: GetCustomersByCountry
+        [HttpGet]
+        [Route("api/customers/by-country/{country}")]
+        public IHttpActionResult GetCustomersByCountry(string country)
+        {
+            var customers = db.GetCustomersByCountry(country).ToList();
+            return Ok(customers);
+        }
+    }
+}
---
+++ webAPI/CC/customerAPI/Models/OrderDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Models/OrderDTO.cs
+namespace CustomerAPI.Models
+{
+    public class OrderDTO
+    {
+        public int OrderID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string CompanyName { get; set; }
+        public string ShipCountry { get; set; }
+    }
+}
